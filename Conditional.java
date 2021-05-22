6a0ee86c7098f872c568964ca672afb0a564cb15
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-05-22 09:47:44 +0530

    Add files via upload

---
+++ Conditional.java
@@ -0,0 +1,11 @@
+public class Conditional 
+{
+public static void main(String[] args)
+{
+int A=10;
+int B=20;
+String result;
+result=A>B? "A is greater":"B is greater";
+System.out.println(result);
+}
+}
